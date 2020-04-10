df697488afb67bc8669befc3d1bac8cf0e6fdb6a
Simrun Basuita <hello@sim.run>
2020-04-10 10:27:48 +0100

    Stop using ENO as index

--- coal.py
+++ coal.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 # ENO is the Entity Number defined by Geoscience Australia
-mines = pd.read_excel("data/operating_mines_feb2015.xlsx", index_col=0)
+mines = pd.read_excel("data/operating_mines_feb2015.xlsx")
 
 for _, mine in mines.iterrows():
-    print(mine.name, mine.loc["Longitude"], mine.loc["Latitude"])
+    print(mine.loc["ENO"], mine.loc["Longitude"], mine.loc["Latitude"])
