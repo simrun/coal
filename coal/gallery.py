509f786e4f39ffc7c6788fcf33a6add9bcb9eb59
Simrun Basuita <hello@sim.run>
2020-04-13 13:40:51 +0100

    Put tiles in their own <div>

--- coal/gallery.py
+++ coal/gallery.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     for _, mine in mines.coal_mines.iterrows():
         ENO = mine["ENO"]
         img_tag = soup.new_tag("img", src=f"{tiles.path}/{ENO}.jpg")
-        soup.body.append(img_tag)
+        soup.body.find(id="tiles").append(img_tag)
 
     soup.body.h1.string = str(len(mines.coal_mines)) + " Australian coal mines"
 
