ea840ba4c30fff727e51723d79798f64ea092a19
Simrun Basuita <hello@sim.run>
2020-04-29 20:11:13 +0100

    Show high interest mines first

--- coal/gallery.py
+++ coal/gallery.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     with open("templates/index.html", "r") as f:
         soup = BeautifulSoup(f, features="html.parser")
 
-    for _, mine in mines.coal_mines.iterrows():
+    for _, mine in mines.coal_mines.sort_values("Interest").iterrows():
+        # Sorting gives us "High" interest mines first
         ENO = mine["ENO"]
         img_tag = soup.new_tag("img", src=f"{tiles.path}/{ENO}.jpg")
         soup.find("main").append(img_tag)
