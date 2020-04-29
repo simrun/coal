a96122af36aaabb1eb18467e42c4d7a0c321eb2f
Simrun Basuita <hello@sim.run>
2020-04-29 19:55:24 +0100

    Only show High and Med interest mines

--- coal/mines.py
+++ coal/mines.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 
 mines = pd.read_excel("data/operating_mines_feb2015.xlsx")
-coal_mines = mines.loc[mines["Commodities"].str.contains("coal", case=False)]
+coal_mines = mines[mines["Commodities"].str.contains("coal", case=False)]
 
 zoom_levels = pd.read_csv("curation.csv")
 
 coal_mines = pd.merge(coal_mines, zoom_levels, how='left', on="ENO", validate="1:1")
+
+coal_mines = coal_mines[coal_mines["Interest"].isin({"High", "Med"})]
