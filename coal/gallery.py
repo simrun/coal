5c8c2120eea3a3c1870c3c037b1d050e45f26de6
Simrun Basuita <hello@sim.run>
2020-04-29 20:19:45 +0100

    Refactor: create add_tile()

--- coal/gallery.py
+++ coal/gallery.py
@@ -2,15 +2,18 @@ from bs4 import BeautifulSoup
 
 from . import config, mines, tiles
 
+def add_tile(soup, mine):
+    ENO = mine["ENO"]
+    img_tag = soup.new_tag("img", src=f"{tiles.path}/{ENO}.jpg")
+    soup.find("main").append(img_tag)
+
 if __name__ == "__main__":
     with open("templates/index.html", "r") as f:
         soup = BeautifulSoup(f, features="html.parser")
 
     for _, mine in mines.coal_mines.sort_values("Interest").iterrows():
         # Sorting gives us "High" interest mines first
-        ENO = mine["ENO"]
-        img_tag = soup.new_tag("img", src=f"{tiles.path}/{ENO}.jpg")
-        soup.find("main").append(img_tag)
-
+        add_tile(soup, mine)
+        
     with open(f"{config.web_root}/index.html", "w") as f:
         f.write(soup.prettify())
