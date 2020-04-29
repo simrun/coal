64d3d21a715416ee677b078d4a7cae5407840169
Simrun Basuita <hello@sim.run>
2020-04-29 20:54:49 +0100

    Variable renaming

--- coal/mines.py
+++ coal/mines.py
@@ -4,8 +4,7 @@ import pandas as pd
 mines = pd.read_excel("data/operating_mines_feb2015.xlsx")
 coal_mines = mines[mines["Commodities"].str.contains("coal", case=False)]
 
-zoom_levels = pd.read_csv("curation.csv")
-
-coal_mines = pd.merge(coal_mines, zoom_levels, how='left', on="ENO", validate="1:1")
+curation = pd.read_csv("curation.csv")
+coal_mines = pd.merge(coal_mines, curation, how='left', on="ENO", validate="1:1")
 
 coal_mines = coal_mines[coal_mines["Interest"].isin({"High", "Med"})]
